fb9e69fd6567b920e97822d675d897dc28629a39
youngflysky <li.liang.zi@qq.com>
2022-04-15 18:33:30 +0800

    1

---
+++ ZhengLiCppCode/Comprehensive example/实验五/client.h
@@ -0,0 +1,14 @@
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+class Client
+{
+	static char ServerName;
+	static int ClientNum;
+
+public:
+	static void ChangeServerName(char name);
+	static int getClientNum();
+};
+
+#endif // CLIENT_H_
