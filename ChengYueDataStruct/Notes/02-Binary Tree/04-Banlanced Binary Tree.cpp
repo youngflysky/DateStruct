4bde80b02ffcd49deff88008002d628fa4781dcc
youngflysky <li.liang.zi@qq.com>
2022-04-29 17:05:38 +0800

    AVL Tree

---
+++ ChengYueDataStruct/Notes/02-Binary Tree/04-Banlanced Binary Tree.cpp
@@ -0,0 +1,14 @@
+// TITLE:Banlanced Binary Tree   (AVL 树)
+
+// README:def：一种特殊的二叉排序树，其任一节点的左、右子树的绝对值不超过 1 。
+
+// NOTE:给定节点数为 n 的 AVL 树的最大高度为 O(lg2(n));
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
