2f9de3b2bb7c63c3708f1b652947adf750a44e7e
youngflysky <li.liang.zi@qq.com>
2022-05-02 16:18:23 +0800

    Root Of AVL Tree

--- ChengYueDataStruct/PTA/12-Root Of AVL Tree.cpp
+++ ChengYueDataStruct/PTA/12-Root Of AVL Tree.cpp
@@ -3,203 +3,170 @@
 #include <vector>
 using namespace std;
 
-typedef struct TNode *BinTree;
-typedef struct TNode
+typedef class AVL_Node *BinTree, TNode;
+class AVL_Node
 {
+public:
     int data;
-    BinTree father;
     BinTree left;
     BinTree right;
-} TNode;
+    int height;
 
-bool IsLeaves(BinTree BST)
+public:
+    AVL_Node(int x) : data(x), left(NULL), right(NULL), height(0){};
+};
+
+// NOTE:求树的深度函数
+int GetHeight(BinTree T)
 {
-    if (!BST)
+    if (T)
     {
-        return true;
-    }
-    if (!BST->right && !BST->left)
-    {
-        return true;
+        int HL = GetHeight(T->left);
+        int HR = GetHeight(T->right);
+        int MaxH = (HL > HR) ? HL : HR;
+        return MaxH + 1;
     }
     else
     {
-        return false;
+        return 0;
     }
 }
 
-bool IsAVL(BinTree BST) //判断是否是平衡二叉树
+// NOTE:RR调整【 左单旋转 】
+BinTree SingleLeftRotation(BinTree T)
 {
-    if (!BST)
-    {
-        return true;
-    }
-    else if (IsLeaves(BST))
-    {
-        return true;
-    }
-    else if (!BST->right)
-    {
-        if (IsLeaves(BST->left))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    else if (!BST->left)
-    {
-        if (IsLeaves(BST->right))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    else if (IsLeaves(BST->left))
-    {
-        if (IsLeaves(BST->right->left) && IsLeaves(BST->right->right))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    else if (IsLeaves(BST->right))
-    {
-        if (IsLeaves(BST->left->left) && IsLeaves(BST->left->right))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    else
-    {
-        return (IsAVL(BST->left) && IsAVL(BST->right));
-    }
+    /*
+            T
+             \
+              B
+             / \
+                 C
+    */
+    // 此时根节点是 T
+    BinTree Root = T->right;
+    T->right = Root->left;
+    Root->left = T;
+    T->height = GetHeight(T);
+    Root->height = GetHeight(Root);
+    return Root;
+    /*
+        B
+       / \
+      T   C
+       \
+    */
 }
 
-void ReBanlanceAVL(BinTree &BST) //调整平衡二叉树;
+// NOTE:LL调整【 右单旋转 】
+BinTree SingleRightRotation(BinTree T)
 {
-    if (!BST)
-    {
-        return;
-    }
-    if (BST->left && !BST->right)
-    {
-        if (BST->left->left)
-        {
-            BinTree Root = BST->left;
-            BST->left->father = BST->father;
-            BST->left->right = BST;
-            BST->left = NULL;
-            BST = Root;
-        }
-        else
-        {
-            BinTree Temp = BST->left->right;
-            BinTree Root = BST->left;
-            Root->left = BST;
-            Root->right = BST->father;
-            Root->father = BST->father->father;
-            Root->left->father = Root->right->father = Root;
-            Root->right->left = Root->left->left = NULL;
-            Root->left->right = Temp;
-        }
-    }
-    else if (BST->right && !BST->left)
-    {
-        if (BST->right->left)
-        {
-            BinTree Temp = BST->right->left;
-            BinTree Root = BST->right;
-            Root->right = BST;
-            Root->left = BST->father;
-            Root->father = BST->father->father;
-            Root->left->father = Root->right->father = Root;
-            Root->right->right = Root->left->right = NULL;
-            Root->right->left = Temp;
-        }
-        else
-        {
-            BinTree Root = BST->right;
-            BST->right->father = BST->father;
-            BST->right->left = BST;
-            BST->right = NULL;
-            BST = Root;
-        }
-    }
-    else
-    {
-        ReBanlanceAVL(BST->left);
-        ReBanlanceAVL(BST->right);
-    }
+    /*
+            T
+           /
+          B
+         / \
+        C
+    */
+    // 此时根节点是 A
+    BinTree Root = T->left;
+    T->left = Root->right;
+    Root->right = T;
+    T->height = GetHeight(T);
+    Root->height = GetHeight(Root);
+    return Root;
+    /*
+        B
+       / \
+      C   T
+         /
+    */
 }
 
-BinTree Insert(int data, BinTree BST) // NOTE:平衡二叉树节点插入时，增加一个 父指针 指向 父节点，根节点父指针为空
+// NOTE:LR调整【先左后右双旋转】
+BinTree DoubleLeftRightRotation(BinTree T)
 {
-    if (!BST)
-    {
-        BST = new TNode;
-        BST->father = BST->left = BST->right = NULL;
-        BST->data = data;
-    }
-    else if (data > BST->data)
-    {
-        BST->right = Insert(data, BST->right);
-        BST->right->father = BST;
-    }
-    else if (data < BST->data)
-    {
-        BST->left = Insert(data, BST->left);
-        BST->left->father = BST;
-    }
-    return BST;
+    T->left = SingleLeftRotation(T->left);
+    return SingleRightRotation(T);
 }
 
-BinTree CreateBST() //建树函数
+// NOTE:RL调整【先右后左双旋转】
+BinTree DoubleRightLeftRotation(BinTree T)
+{
+    T->right = SingleRightRotation(T->right);
+    return SingleLeftRotation(T);
+}
+
+// NOTE: AVL 在节点插入的同时实时维护 AVL 的平衡;
+
+BinTree AVL_Insert(int data, BinTree T)
+{
+    if (!T)
+    {
+        T = new TNode(data);
+    }
+    else if (data > T->data)
+    {
+        T->right = AVL_Insert(data, T->right);
+        // NOTE:新插入节点在右子树上，所以一定是右子树深度大于左子树
+        if (GetHeight(T->right) - GetHeight(T->left) == 2)
+        {
+            if (data < T->right->data) // README:RL
+            {
+                T = DoubleRightLeftRotation(T);
+            }
+            else if (data > T->right->data) // README:RR
+            {
+                T = SingleLeftRotation(T);
+            }
+        }
+    }
+    else if (data < T->data)
+    {
+        // NOTE:新插入节点在右子树上，所以一定是左子树深度大于右子树
+        T->left = AVL_Insert(data, T->left);
+        if (GetHeight(T->left) - GetHeight(T->right) == 2)
+        {
+            if (data < T->left->data) // README:LL
+            {
+                T = SingleRightRotation(T);
+            }
+            else if (data > T->left->data) // README:LR
+            {
+                T = DoubleLeftRightRotation(T);
+            }
+        }
+    }
+    T->height = GetHeight(T);
+    return T;
+}
+
+BinTree Create_AVL() //建树函数
 {
     int N;
     cin >> N;
-    int data;
-    BinTree BST = NULL;
+    int data = 0;
+    BinTree avl = NULL;
     for (int i = 0; i < N; ++i)
     {
         cin >> data;
-        BST = Insert(data, BST);
-        if (!IsAVL(BST))
-        {
-            ReBanlanceAVL(BST);
-        }
+        avl = AVL_Insert(data, avl);
     }
-    return BST;
+    return avl;
 }
-
-void DeleteBT(BinTree BST) //释放BST;
+void DeleteBT(BinTree T) //后序遍历释放内存
 {
-    if (BST)
+    if (T)
     {
-        DeleteBT(BST->left);
-        DeleteBT(BST->right);
-        delete BST;
-        BST = NULL;
+        DeleteBT(T->left);
+        DeleteBT(T->right);
+        delete T;
     }
     return;
 }
 
 int main()
 {
-    BinTree BST = CreateBST();
-    cout << BST->data;
-    DeleteBT(BST);
+    BinTree AVL = Create_AVL();
+    cout << AVL->data;
     return 0;
 }
