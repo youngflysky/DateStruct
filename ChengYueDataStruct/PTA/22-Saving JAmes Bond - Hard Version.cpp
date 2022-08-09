200211b33e35460d94013c50af4d748b9e5c1419
youngflysky <li.liang.zigod@gmail.com>
2022-08-09 16:25:09 +0800

    22-Saving JAmes Bond - Hard Version

--- ChengYueDataStruct/PTA/22-Saving JAmes Bond - Hard Version.cpp
+++ ChengYueDataStruct/PTA/22-Saving JAmes Bond - Hard Version.cpp
@@ -1,212 +1,153 @@
+// TITLE:22-Saving JAmes Bond - Hard Version
 #include <cmath>
 #include <iostream>
 #include <queue>
-#include <vector>
 using namespace std;
-#define MaxSize 105
-#define RADIUS 7.5
-#define INF 105
+#define MaxSize 101
+#define DIAMETER 15
+#define INF 101
 
 int Nv, Jump;
 
-class Point
+typedef struct GraphNode
 {
-private:
-    static int Jump;
-
-public:
-    static void IniteJump(int parameter = 0)
-    {
-        Jump = parameter;
-        return;
-    }
-
-public:
     int x, y;
-
-    int Distance_Origin()
-    {
-        return x * x + y * y;
-    }
-
-public:
-    int dist[MaxSize];
-    int path[MaxSize] = {0};
-
-public:
-    Point() : x(0), y(0){};
-    ~Point(){};
-
-    void InputPoint()
-    {
-        cin >> x >> y;
-        if (x * x >= 50 * 50 || y * y >= 50 * 50 || (x * x + y * y) < RADIUS * RADIUS)
-        {
-            x = y = INF;
-        }
-        return;
-    }
+    bool firstV, lastV;
 
     bool IsFirstV()
     {
-        if ((RADIUS + Jump) * (RADIUS + Jump) >= (x * x + y * y))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return x * x + y * y <= (Jump + DIAMETER / 2) * (Jump + DIAMETER / 2);
     }
-
-    void IniteDist(int s)
+    bool IsLastV()
     {
-
-        for (int i = 0; i < Nv; ++i)
-        {
-            dist[i] = -1;
-        }
-        dist[s] = 1;
+        return abs(x) + Jump >= 50 || abs(y) + Jump >= 50;
     }
+} GraphNode;
+GraphNode GNode[MaxSize];
+int Dist[MaxSize], Path[MaxSize], Pre[MaxSize];
 
-    bool ExistEdge(Point &i)
+bool Cmp(int s1, int s2) //比较两个起点谁距离小岛更近;
+{
+    return GNode[s1].x * GNode[s1].x + GNode[s1].y * GNode[s1].y > GNode[s2].x * GNode[s2].x + GNode[s2].y * GNode[s2].y;
+}
+bool CanJump(int p1, int p2) //判断 James 是否可以从 P1 跳到 P2;
+{
+    return (GNode[p1].x - GNode[p2].x) * (GNode[p1].x - GNode[p2].x) + (GNode[p1].y - GNode[p2].y) * (GNode[p1].y - GNode[p2].y) <= Jump * Jump;
+}
+void IniteDist(int s) //初始化 Dist 数组, 到自身距离为0，到其他点距离为一个 特殊值（无法取到的值）;
+{
+    for (int i = 0; i < Nv; ++i)
     {
-        if (pow(i.x - x, 2) + pow(i.y - y, 2) < pow(Jump, 2))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        Dist[i] = -1;
     }
-
-    bool JumpBank()
+    Dist[s] = 0;
+}
+void CopyPath() //将临时“路径数组 Pre 复制到 Path 中”;
+{
+    for (int i = 0; i < Nv; ++i)
     {
-        if (x + Jump >= 50 || x - Jump <= -50 || y + Jump >= 50 || y - Jump <= -50)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        Path[i] = Pre[i];
     }
-
-    void FindPath(int initeV, int lastV, vector<int> &result)
+}
+void PrintPath(int s, int w) // NOTE:递归逆序打印 “路径数组”;
+{
+    if (s == w)
     {
-        if (path[lastV] == initeV)
-        {
-            result.push_back(lastV);
-            result.push_back(initeV);
-            return;
-        }
-        else
-        {
-            result.push_back(lastV);
-            lastV = path[lastV];
-            FindPath(initeV, lastV, result);
-        }
+        cout << GNode[s].x << ' ' << GNode[s].y << endl;
+        return;
     }
-};
-int Point::Jump = 0;
-Point G[MaxSize];
+    PrintPath(s, Path[w]);
+    cout << GNode[w].x << ' ' << GNode[w].y << endl; // NOTE:当打印至于递归入口之前时则为顺序打印;
+}
 
 void BuildGraph()
 {
-    cin >> Nv >> Jump;
-    Point::IniteJump(Jump);
     for (int i = 0; i < Nv; ++i)
     {
-        G[i].InputPoint();
+        cin >> GNode[i].x >> GNode[i].y;
+        GNode[i].firstV = GNode[i].IsFirstV();
+        GNode[i].lastV = GNode[i].IsLastV();
     }
 }
 
-int MinDist_UnweighedG(int s, int &lastV);
-
-void SavingJames()
+void ShortestPath_UnweighedG(int s) // NOTE:无向无权图的单源最短路径算法;
 {
-    int minDist = INF, initeV = 0, lastV = 0;
-    int tempDist, tempLastV;
-    for (int i = 0; i < Nv; ++i)
-    {
-        if (!G[i].IsFirstV())
-        {
-            continue;
-        }
-        tempDist = MinDist_UnweighedG(i, tempLastV);
-        if (tempDist == INF)
-        {
-            continue;
-        }
-        else if (minDist > tempDist)
-        {
-            minDist = tempDist;
-            initeV = i;
-            lastV = tempLastV;
-        }
-        else if (minDist == tempDist)
-        {
-            if (G[initeV].Distance_Origin() > G[i].Distance_Origin())
-            {
-                initeV = i;
-                lastV = tempLastV;
-            }
-        }
-    }
-    if (minDist == INF)
-    {
-        cout << '0' << endl;
-    }
-    else
-    {
-        cout << minDist << endl;
-        vector<int> result;
-        G[initeV].FindPath(initeV, lastV, result);
-        for (int i = result.size() - 1; i >= 0; --i)
-        {
-            cout << G[result[i]].x << ' ' << G[result[i]].y << endl;
-        }
-    }
-}
-
-int MinDist_UnweighedG(int s, int &tempLastV)
-{
-    G[s].IniteDist(s);
+    IniteDist(s);
     queue<int> que;
-    int v_ing;
     que.push(s);
+    int v;
     while (!que.empty())
     {
-        v_ing = que.front();
-        if (G[v_ing].JumpBank())
-        {
-            tempLastV = v_ing;
-            return G[s].dist[v_ing] + 1;
-            break;
-        }
+        v = que.front();
         que.pop();
         for (int w = 0; w < Nv; ++w)
         {
-            if (G[s].dist[w] == -1 && G[v_ing].ExistEdge(G[w]))
+            if (Dist[w] == -1 && CanJump(v, w))
             {
-                G[s].dist[w] = G[s].dist[v_ing] + 1;
-                G[s].path[w] = v_ing;
+                Dist[w] = Dist[v] + 1;
+                Pre[w] = v;
                 que.push(w);
             }
         }
     }
-    return INF;
+}
+
+void Saving007()
+{
+    int minDist = INF; //记录最短路径
+    int S, W;          // S--起点，W--终点;
+    for (int s = 0; s < Nv; ++s)
+    {
+        if (!GNode[s].firstV)
+        {
+            continue;
+        }
+        ShortestPath_UnweighedG(s);
+
+        for (int w = 0; w < Nv; ++w)
+        {
+            if (!GNode[w].lastV || Dist[w] == -1)
+            {
+                continue;
+            }
+            if (minDist > Dist[w])
+            {
+                minDist = Dist[w];
+                S = s;
+                W = w;
+                CopyPath();
+            }
+            else if (minDist == Dist[w] && Cmp(S, s))
+            {
+                S = s;
+                W = w;
+                CopyPath();
+            }
+        }
+    }
+
+    if (minDist == INF)
+    {
+        cout << 0;
+    }
+    else
+    {
+        cout << Dist[W] + 2 << endl;
+        PrintPath(S, W);
+    }
+
+    return;
 }
 
 int main()
 {
+    cin >> Nv >> Jump;
     BuildGraph();
-    if (Jump + RADIUS >= 50)
+    if (Jump + DIAMETER / 2 >= 50)
     {
-        cout << "1" << endl;
+        cout << 1;
         return 0;
     }
-    SavingJames();
+    Saving007();
     return 0;
 }
