000bb14cf09e36243639e2fccfacb096f3b1699a
youngflysky <li.liang.zigod@gmail.com>
2022-10-03 17:16:56 +0800

    01-MaxSubSeqSum

---
+++ PTA/01-The Largest SubSequence.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+// NOTE:贪心算法;
+int MaxSubSeqSum(int A[], int N)
+{
+    int ThisSum = 0, MaxSum = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        ThisSum += A[i];
+        MaxSum = ThisSum > MaxSum ? ThisSum : MaxSum;
+        if (ThisSum < 0)
+        {
+            ThisSum = 0;
+        }
+    }
+    return MaxSum;
+}
+
+int main()
+{
+    int N;
+    int *A;
+    cin >> N;
+    A = new int[N];
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> A[i];
+    }
+
+    cout << MaxSubSeqSum(A, N);
+
+    delete (A);
+
+    return 0;
+}
