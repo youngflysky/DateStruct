0360756158366e199d8fd673027ef07a3cf1416a
youngflysky <li.liang.zigod@gmail.com>
2022-10-30 16:57:52 +0800

    旅游规划--Dijkstra 算法

---
+++ PTA/20-Travel Plan.cpp
@@ -0,0 +1,103 @@
+// TITLE:旅游规划--Dijkstra 算法;
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+using namespace std;
+
+const int MaxSzie = 505;
+const int INF = 0XFFFFFF;
+
+int Nv, Ne;
+int S, D;
+
+typedef struct GraphVal
+{
+    int dist;
+    int price;
+    GraphVal() : dist(INF), price(INF) {}
+} GraphVal;
+GraphVal G[MaxSzie][MaxSzie]; // README:矩阵的一对坐标队定位一条边;
+
+int dist[MaxSzie], price[MaxSzie];
+bool collected[MaxSzie];
+void Inite_dist_path_collected()
+{
+    for (int i = 0; i < Nv; ++i)
+    {
+        dist[i] = price[i] = INF;
+
+        collected[i] = false;
+    }
+}
+
+void BuildGraph()
+{
+    cin >> Nv >> Ne >> S >> D;
+    for (int i = 0, v, w, d, p; i < Ne; ++i)
+    {
+        cin >> v >> w >> d >> p;
+        G[w][v].dist = G[v][w].dist = d, G[w][v].price = G[v][w].price = p;
+    }
+}
+
+void Dijkstra(int s) // NOTE:Dijkstra:单源有权图最短路径算法;
+{
+    Inite_dist_path_collected();
+
+    dist[s] = 0;  // NOTE:主权;
+    price[s] = 0; // NOTE:副权
+
+    int minE;
+    int v;
+    while (true)
+    {
+        minE = INF;
+        for (int i = 0; i < Nv; ++i)
+        {
+            if (!collected[i] && dist[i] < minE)
+            {
+                minE = dist[i];
+                v = i;
+            }
+        }
+        if (minE == INF) // README:未收集节点中没有最小dist时，即为结束条件;
+        {
+            return;
+        }
+        collected[v] = true;
+
+        for (int w = 0; w < Nv; ++w)
+        {
+            if (!collected[w] && G[v][w].dist < INF)
+            {
+                if (dist[w] > dist[v] + G[v][w].dist)
+                {
+                    dist[w] = dist[v] + G[v][w].dist;
+                    price[w] = price[v] + G[v][w].price;
+                }
+                else if (dist[w] == dist[v] + G[v][w].dist && price[w] > price[v] + G[v][w].price)
+                {
+                    price[w] = price[v] + G[v][w].price;
+                }
+            }
+        }
+    }
+}
+
+void TravelPlan()
+{
+    Dijkstra(S);
+    cout << dist[D] << ' ' << price[D];
+}
+
+int main()
+{
+    BuildGraph();
+    TravelPlan();
+    return 0;
+}
